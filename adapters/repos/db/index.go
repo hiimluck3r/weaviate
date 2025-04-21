f81cb098054e13f31ab1c01da463d73ab6b00e7d
Jeronimo <jeronimo.irazabal@gmail.com>
2025-04-21 18:38:33 -0300

    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: add explanatory comment
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>
    
    chore: possibility to specific an inactivity timeout to ensure maintenance tasks are resumed
    
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- adapters/repos/db/index.go
+++ adapters/repos/db/index.go
@@ -668,6 +668,7 @@ type IndexConfig struct {
 	AvoidMMap                           bool
 	DisableLazyLoadShards               bool
 	ForceFullReplicasSearch             bool
+	TransferInactivityTimeout           time.Duration
 	LSMEnableSegmentsChecksumValidation bool
 	TrackVectorDimensions               bool
 	ShardLoadLimiter                    ShardLoadLimiter
